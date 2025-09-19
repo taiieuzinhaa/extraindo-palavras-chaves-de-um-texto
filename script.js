16a71fd3df11d9da588924a68c92a28a6a2be5bd
taiieuzinhaa <schmeing.estephani@escola.pr.gov.br>
2025-09-19 08:33:52 -0300

    ok

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
 const texto = document.querySelector('#entrada-de-texto').value;
-
 const campoResultado = document.querySelector('#resultado-
 palavrachave');
-
 const palavrasChave = processaTexto(texto);
+
 campoResultado.textContent = palavrasChave.join(", ");
 }
 function processaTexto(texto) {
 let palavras = texto.split(/\P{L}+/u);
+let frequencias = {};
+for (let i of palavras) {
+    frequencias[i] = 0;
+for (let j palavras) {
+    if (i == j) {
+        frequencias[1]++;
+    }
+        
+    }
+}
+}
+console.log(frequencias);
 return palavras;
 }
